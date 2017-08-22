42be0ac2352a76a6af9d229a8e25a5f2862e4572
alexeikorzhov <aliaksei_karzhou@epam.com>
2017-08-22 13:24:00 +0300

    tests

--- task1_3/task1_jboss/test/smoke/default/default_test.rb
+++ task1_3/task1_jboss/test/smoke/default/default_test.rb
@@ -16,3 +16,29 @@ end
 describe port(80), :skip do
   it { should_not be_listening }
 end
+
+describe package('java-1.8.0-openjdk-devel') do
+  it { should be_installed }
+end
+
+describe file('/opt/wildfly') do
+ its('type') { should eq :directory }
+ it { should be_directory }
+end
+
+describe file('/opt/wildfly/standalone/deployments/helloworld.war') do
+ it { should exist }
+end
+
+describe file('/etc/systemd/system/wildfly.service') do
+ it { should exist }
+end
+
+describe service('wildfly') do
+    it { should be_enabled }
+    it { should be_running }
+end
+
+describe http('http://192.168.56.12:8080/helloworld/HelloWorld') do
+  its('status') { should eq 200 }
+end
